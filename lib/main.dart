7ab3f2cfe10bf994d26fbf1c4cbd8c195136fc8c
Arash Farhadi <mr2c2r@gmail.com>
2023-09-25 20:15:09 +0330

    implement splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,25 +15,44 @@ class Application extends StatelessWidget {
   }
 }
 
+//splash Screen
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Color(0xff1C1F2E),
-      backgroundColor: Colors.transparent,
+      backgroundColor: Color(0xff1C1F2E),
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage('images/pattern1.png'),
               repeat: ImageRepeat.repeatY),
         ),
-        child: Center(
-          child: Text(
-            'arash',
-            style: TextStyle(color: Colors.white),
-          ),
+        child: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 80),
+                  child: Image(
+                    image: AssetImage('images/logo_splash.png'),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+                bottom: 32,
+                child: Column(
+                  children: [
+                    Text('Powered by',
+                        style: TextStyle(color: Colors.white.withOpacity(0.3))),
+                    Text('NoroNet Group',
+                        style: TextStyle(color: Color(0xff55B9F7))),
+                  ],
+                ))
+          ],
         ),
       ),
     );
